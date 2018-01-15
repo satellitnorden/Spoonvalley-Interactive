b44e251494f51331ee26903120925535306badcb
Daybreak <DennisMartenssons@gmail.com>
2018-01-15 19:39:20 +0100

    Removed unnecessary stuff in the intermediate and saved folders as these should not be in the repository. (:

--- Unreal Project/DrunkenDiscoPirates/Intermediate/Build/Win64/UE4Editor/Development/DrunkenDiscoPirates/PCH.DrunkenDiscoPirates.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Dennis/Documents/GitHub/Spoonvalley-Interactive/Unreal Project/DrunkenDiscoPirates/Intermediate/Build/Win64/UE4Editor/Development/DrunkenDiscoPirates/PCH.DrunkenDiscoPirates.h"
