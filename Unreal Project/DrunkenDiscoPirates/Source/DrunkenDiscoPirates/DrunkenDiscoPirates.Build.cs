b3bce771c855a4378380eb252e0cfd5c8b8ccb60
Daybreak <DennisMartenssons@gmail.com>
2018-01-16 22:53:19 +0100

    Final implementation of rum meter. : D

--- Unreal Project/DrunkenDiscoPirates/Source/DrunkenDiscoPirates/DrunkenDiscoPirates.Build.cs
+++ Unreal Project/DrunkenDiscoPirates/Source/DrunkenDiscoPirates/DrunkenDiscoPirates.Build.cs
@@ -6,9 +6,10 @@ public class DrunkenDiscoPirates : ModuleRules
 {
 	public DrunkenDiscoPirates(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "UMG", "Slate", "SlateCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
